2cbad921c5a113f0bf3f8995352b22f66bbe1016
bee-dmg <brandonschmersal@gmail.com>
2021-11-28 18:40:08 -0500

    Firebase/ENV

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import {createUserWithEmailAndPassword } from "firebase/auth";
-import {auth} from './firebase-config';
+import {auth, firebaseObject} from './firebase-config';
 
 export default function Login() {
   const styles = StyleSheet.create({
@@ -29,14 +29,16 @@ export default function Login() {
   const [registerPassword, setRegisterPassword] = useState("");
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
- 
+ console.log(firebaseObject);
+ console.log(process.env.REACT_APP_API_KEY)
   const register = async () => {
 try{
    const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+   console.log(user);
   } catch(error){
     console.log(error.message);
   }
-  }
+  };
   const login = async () => {
 
   }
@@ -63,7 +65,7 @@ try{
         <TextInput editable style={styles.input} placeholder="Password" onChange={(event) => {setLoginPassword(event.target.value)}} />
       </View>
       <View style={styles.fixToText}>
-        <Button title="Sign Up" onClick={register} /> 
+        <Button title="Sign Up" onClick={register()} /> 
         <Button title="Login" /> 
       </View>
 
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,19 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+//import { REACT_APP_API_KEY, REACT_APP_AUTH_DOMAIN, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_SENDER_ID, REACT_APP_APP_ID, REACT_APP_MEASUREMENT_ID } from "react-native-dotenv";
 
 const firebaseConfig = {
 
-    apiKey: `${process.env.REACT_APP_API_KEY}`,
-    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-    storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
-    messagingSenderId: `${process.env.REACT_APP_SENDER_ID }`,
-    appId: `${process.env.REACT_APP_APP_ID}`,
-    measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_SENDER_ID ,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   
   };
   
 
   const app = initializeApp(firebaseConfig);
 
- export const auth = getAuth(app);
+//export const auth = getAuth(app);
+
+export const firebaseObject = firebaseConfig;
