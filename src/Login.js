28df2f2ad637b40861c909607fa14101a2e2f3d2
bee-dmg <brandonschmersal@gmail.com>
2021-11-28 19:56:16 -0500

    Authentication working

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
-import {createUserWithEmailAndPassword } from "firebase/auth";
-import {auth, firebaseObject} from './firebase-config';
+import {createUserWithEmailAndPassword, onAuthStateChanged,signOut, signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from './firebase-config';
 
 export default function Login() {
   const styles = StyleSheet.create({
@@ -25,10 +25,16 @@ export default function Login() {
     }
   });
 
+
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [user, setUser] = useState("");
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  })
   const register = async () => {
 try{
    const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
@@ -38,10 +44,20 @@ try{
   }
   };
   const login = async () => {
-
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
   const logout = async () => {
+    await signOut(auth);
 
   }
   return (
@@ -63,11 +79,12 @@ try{
         <TextInput editable style={styles.input} placeholder="Password" onChange={(event) => {setLoginPassword(event.target.value)}} />
       </View>
       <View style={styles.fixToText}>
-        <Button title="Sign Up" onClick={register }/> 
-        <Button title="Login" /> 
+        <Button title="Sign Up" onPress={register}/> 
+        <Button title="Login" onPress={login} /> 
       </View>
 
-      <View style={styles.center}><Button title="Sign Out"  /></View>
+      <View style={styles.center}><Button title="Sign Out" onPress={logout} /></View>
+      <View> {user?.email}</View>
     </View>
   );
 }
