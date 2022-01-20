a05546b8bd344f20a5cb6cd13a8c377c33370a48
bee-dmg <brandonschmersal@gmail.com>
2022-01-19 19:32:24 -0500

    User sign up, actually working

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 import Login from "./login/Login";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { startClock } from "react-native-reanimated";
-import Home from "./Home";
+
 
 export default function Main() {
-  const Stack = createNativeStackNavigator();
+
   return (
     <Login />
  
